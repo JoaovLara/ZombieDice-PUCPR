1f60187e8cdfac08386f264804b837cd44da117d
João Lara <117029722+JoaovLara@users.noreply.github.com>
2023-08-18 14:45:45 -0300

    Update ZombieDice.py
    Correção de indentação e play_game

--- ZombieDice.py
+++ ZombieDice.py
@@ -1,28 +1,22 @@
-"""
-ZOMBIE DICE Prototipo FINAL
-João Vitor de Lara
-Raciocínio Computacional
-"""
-
 from random import randint
 
-def CorDado(numSorteado):
-    if numSorteado <= 5:
+def cor_dado(num_sorteado):
+    if num_sorteado <= 5:
         return "verde"
-    elif numSorteado <= 9:
+    elif num_sorteado <= 9:
         return "amarelo"
     else:
         return "vermelho"
 
-def VerificarGanhador(listaJogadores, ListaCerebros, numJogadores):
-    for i in range(numJogadores):
-        if ListaCerebros[i] >= 13:
-            print('O jogador', listaJogadores[i], 'Venceu')
+def verificar_ganhador(lista_jogadores, lista_cerebros, num_jogadores):
+    for i in range(num_jogadores):
+        if lista_cerebros[i] >= 13:
+            print('O jogador', lista_jogadores[i], 'Venceu')
             return True
-    if numJogadores > 1:
+    if num_jogadores > 1:
         return False
     else:
-        print('O jogador', listaJogadores[0], 'Venceu')
+        print('O jogador', lista_jogadores[0], 'Venceu')
         print("\n")
         return True
 
@@ -30,89 +24,86 @@ print("ZOMBIE DICE PROJETO FINAL")
 print("SEJA BEM VINDO AO JOGO ZOMBIE DICE")
 print("\n")
 
-numJogadores = 0
-while numJogadores < 2:
-    numJogadores = int(input("Informe o número de jogadores: "))
-    if numJogadores < 2:
-        print('ATENÇÃO! VOCÊ PRECISA DE NO MÍNIMO 2 JOGADORES!')
+num_jogadores = 0
+while num_jogadores < 2:
+    num_jogadores = int(input("Informe o número de jogadores: "))
+    if num_jogadores < 2:
+        print('ATENÇÃO! VOCÊ PRECISA DE PELO MENOS 2 JOGADORES!')
 
-listaJogadores = []
+lista_jogadores = []
 
-for i in range(numJogadores):
-    nome = input('Informe o nome do jogador ' + str(i + 1) + ': ')
-    listaJogadores.append(nome)
+for i in range(num_jogadores):
+    nome = input(f"Informe o nome do jogador {i + 1}: ")
+    lista_jogadores.append(nome)
 
 # Definindo os dados
-dadoVerde = ["C", "P", "C", "T", "P", "C"]
-dadoAmarelo = ["T", "P", "C", "T", "P", "C"]
-dadoVermelho = ["T", "P", "T", "C", "P", "T"]
+dado_verde = ["C", "P", "C", "T", "P", "C"]
+dado_amarelo = ["T", "P", "C", "T", "P", "C"]
+dado_vermelho = ["T", "P", "T", "C", "P", "T"]
 
 # Quantidade dos dados por cores
-listaDados = [
-    dadoVerde, dadoVerde, dadoVerde, dadoVerde, dadoVerde, dadoVerde,
-    dadoAmarelo, dadoAmarelo, dadoAmarelo, dadoAmarelo,
-    dadoVermelho, dadoVermelho, dadoVermelho
+lista_dados = [
+    dado_verde, dado_verde, dado_verde, dado_verde, dado_verde, dado_verde,
+    dado_amarelo, dado_amarelo, dado_amarelo, dado_amarelo,
+    dado_vermelho, dado_vermelho, dado_vermelho
 ]
 
 # Pontuação dos Jogadores
 lista_tiro = []
-ListaCerebros = []
+lista_cerebros = []
 
-for _ in range(numJogadores):
-    ListaCerebros.append(0)
+for i in range(num_jogadores):
+    lista_cerebros.append(0)
     lista_tiro.append(0)
 
-jogadorAtual = 0
-alguemGanhou = False
+jogador_atual = 0
+alguem_ganhou = False
 
 print("\n")
 print('INICIANDO O JOGO...')
 print("\n")
 
 # Sorteando os dados por jogador em cada turno
-while not alguemGanhou:
+while not alguem_ganhou:
     passos = 0
 
-    print("Turno do jogador: {}".format(listaJogadores[jogadorAtual]))
+    print("Turno do jogador:", lista_jogadores[jogador_atual])
     for i in range(3):
-        numSorteado = randint(0, 12)
-        corDado = CorDado(numSorteado)
-        print("Dado sorteado {}: {}".format((i + 1), corDado))
-        dadoSorteado = listaDados[numSorteado]
-        faceDado = randint(0, 5)
-        if dadoSorteado[faceDado] == 'C':
+        num_sorteado = randint(0, 12)
+        cor_do_dado = cor_dado(num_sorteado)  # Renomeie a variável aqui
+        print(f"Dado sorteado {i + 1}: {cor_do_dado}")
+        dado_sorteado = lista_dados[num_sorteado]
+        face_dado = randint(0, 5)
+        if dado_sorteado[face_dado] == 'C':
             print("VOCE COMEU UM CEREBRO!!\n")
-            ListaCerebros[jogadorAtual] += 1
-
-        elif dadoSorteado[faceDado] == "T":
+            lista_cerebros[jogador_atual] += 1
+        elif dado_sorteado[face_dado] == "T":
             print("VOCE LEVOU UM TIRO!!\n")
-            lista_tiro[jogadorAtual] += 1
-            if lista_tiro[jogadorAtual] >= 3:
+            lista_tiro[jogador_atual] += 1
+            if lista_tiro[jogador_atual] >= 3:
                 print("FALECEU")
                 print("\n")
-                del listaJogadores[jogadorAtual]
-                del ListaCerebros[jogadorAtual]
-                numJogadores -= 1
+                del lista_jogadores[jogador_atual]
+                del lista_cerebros[jogador_atual]
+                num_jogadores -= 1
                 passos = 0
                 break
-
         else:
-            print("A vitima escapou!\n")
+            print("A vítima escapou!\n")
             passos += 1
 
     print("\n")
 
     if passos > 0:
-        playGame = input("Jogar dados de novo? (sim / nao): ")
-
-        if playGame == "nao":
+        play_game = input("Jogar dados de novo? (sim / nao): ")
+        if play_game.lower() == "nao":
             print("Continuar jogo!\n")
-            lista_tiro[jogadorAtual] = 0
-            jogadorAtual = (jogadorAtual + 1) % numJogadores
+            lista_tiro[jogador_atual] = 0
+            jogador_atual = (jogador_atual + 1) % num_jogadores
     else:
-        lista_tiro[jogadorAtual] = 0
-        jogadorAtual = (jogadorAtual + 1) % numJogadores
+        lista_tiro[jogador_atual] = 0
+        jogador_atual = (jogador_atual + 1) % num_jogadores
 
-    alguemGanhou = VerificarGanhador(listaJogadores, ListaCerebros, numJogadores)
+    alguem_ganhou = verificar_ganhador(lista_jogadores, lista_cerebros, num_jogadores)
 
 print('Fim do jogo...')
